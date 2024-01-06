162dc34639369e8d7eb8792c367692d82183cc17
PatrykCzartowski <patrykczartowski6@gmail.com>
2024-01-06 22:49:54 +0100

    Restart functionality

--- tic-tac-toe/src/App.jsx
+++ tic-tac-toe/src/App.jsx
@@ -27,7 +27,7 @@ function App() {
 
   const activePlayer = deriveActivePlayer(gameTurns);
 
-  let gameBoard = initialGameBoard;
+  let gameBoard = [...initialGameBoard.map(array => [...array])];
 
   for (const turn of gameTurns) {
     const { square, player } = turn;
@@ -69,6 +69,10 @@ function App() {
     });
   };
 
+  const handleRestart = () => {
+    setGameTurns([]);
+  }
+
   return (
     <main>
       <div id="game-container">
@@ -84,7 +88,7 @@ function App() {
             isActive={activePlayer === "O"}
           />
         </ol>
-        {(winner || hasDraw) && <GameOver winner={winner} />}
+        {(winner || hasDraw) && (<GameOver winner={winner} onRestart={handleRestart}/>)}
         <GameBoard onSelectSquare={handleSelectSquare} board={gameBoard} />
       </div>
       <Log turns={gameTurns} />
